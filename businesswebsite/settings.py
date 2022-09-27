76f68361ec09d5d4d1dc9d7a9f0c5347f5fa1a55
Greg <gregcraig23@gmail.com>
2022-09-26 20:48:27 -0600

    quick fix for changing debug back to false

--- businesswebsite/settings.py
+++ businesswebsite/settings.py
@@ -40,7 +40,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#std-setting-ALLOWED_HOSTSc 
 # something like .example.com will match both www and non www
