ee98529eb2630858c580642c31e14ff1d34f6ef3
Greg <gregcraig23@gmail.com>
2022-10-02 18:35:54 -0600

    turn off debug mode

--- businesswebsite/settings.py
+++ businesswebsite/settings.py
@@ -190,7 +190,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Debug settings
 if (DEBUG):
